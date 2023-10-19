8b8f9bb6fbdeb9ff67f7c3eff7ad3f7a1062fe05
Pareek-Pawan <pareek.pawan2001@gmail.com>
2023-10-19 15:10:00 +0530

    19 October

---
+++ HandsOns/K.py
@@ -0,0 +1,4 @@
+print("Hello Pawan Pareek"[9])
+
+
+print(3.4159)
---
+++ HandsOns/P.py
@@ -0,0 +1,7 @@
+street_name = "Yo"
+print(street_name[0] + street_name[1])
+
+
+num_char = len(input("What's your name?\n"))
+new_num_char = str(num_char)
+print("Your name has " +new_num_char+" Characters.")
