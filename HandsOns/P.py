c70a59a3432eb07a3aa69b1989cfd4866306c256
Pareek-Pawan <pareek.pawan2001@gmail.com>
2023-10-25 14:55:00 +0530

    HandsON

--- HandsOns/P.py
+++ HandsOns/P.py
@@ -1,8 +1,19 @@
-street_name = "Yo"
-print(street_name[0] + street_name[1])
+# street_name = "Yo"
+# print(street_name[0] + street_name[1])
+#
+#
+# num_char = len(input("What's your name?\n"))
+# new_num_char = str(num_char)
+# print("Your name has " +new_num_char+" Characters.")
+#
 
+Number1 = input("Enter number 1:\n")
+Number2 = input("Enter number 2:\n")
 
-num_char = len(input("What's your name?\n"))
-new_num_char = str(num_char)
-print("Your name has " +new_num_char+" Characters.")
+Convert1 = int(Number1)
+Convert2 = int(Number2)
+
+Result = Convert1 + Convert2
+
+print(Result)
 
